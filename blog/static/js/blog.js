bd6f02ce8b0ffb58a2624005f09216328f9bad99
peeyushsahu <peeyushsahu@yahoo.com>
2016-08-12 15:19:43 +0200

    Added genebased result page and linked with output folder.

--- blog/static/js/blog.js
+++ blog/static/js/blog.js
@@ -19,14 +19,3 @@ function readtable4txt(file_path){
     console.log(d3.tsv.parseRows(data));
     });
 }
-
-$(document).ready(function() {
-    $('#example').DataTable( {
-        columns: [
-            { title: "celltype" },
-            { title: "genecluster" },
-            { title: "p-val" },
-            { title: "FDR" },
-        ]
-    } );
-} );
